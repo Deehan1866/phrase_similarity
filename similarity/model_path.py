c02c37ee519123c4c67db4e52eb6f4954a842814
Deehan1866 <105232241+Deehan1866@users.noreply.github.com>
2024-06-18 16:38:09 +0600

    Update model_path.py

--- similarity/model_path.py
+++ similarity/model_path.py
@@ -1,12 +1,16 @@
-
 MODEL_PATH = {
-    "PhraseBERT": "whaleloops/phrase-bert",
+    #"PhraseBERT": "whaleloops/phrase-bert",
     # "DensePhrases": "princeton-nlp/densephrases-multi-query-multi",
-    "BERT": "bert-base-uncased",
-    "SpanBERT-base": "SpanBERT/spanbert-base-cased",
-    "SpanBERT-large": "SpanBERT/spanbert-large-cased",
-    "SentenceBERT": "bert-base-nli-stsb-mean-tokens",
-    "SimCSE": "princeton-nlp/sup-simcse-bert-base-uncased",
+    #"BERT": "bert-base-uncased",
+    #"SpanBERT-base": "SpanBERT/spanbert-base-cased",
+    #"SpanBERT-large": "SpanBERT/spanbert-large-cased",
+    #"SentenceBERT": "bert-base-nli-stsb-mean-tokens",
+    #"SimCSE": "princeton-nlp/sup-simcse-bert-base-uncased",
+    #"E5": "intfloat/e5-base-v2"
+    #"EMistral": "intfloat/e5-mistral-7b-instruct"  #not working
+    #"Mistral": "mistralai/Mistral-7B-v0.1" #same issue as the previous one
+    #"Xlnet": "xlnet/xlnet-base-cased" #OverflowError: can't convert negative int to unsigned
+    #"Albert": "albert/albert-base-v2" #many many phrases are not found! accuracy is really low
+    "T511B": "google-t5/t5-11b"
 }
-
 
